9601df9a5bf3044d1c10cfd9de8f33f6309ee0ba
Leonardo Silva <leoaug1981@hotmail.com>
2026-01-20 17:59:42 -0300

    primeiro commit

--- src/main/java/br/com/exemplo/credito/CreditoApiApplication.java
+++ src/main/java/br/com/exemplo/credito/CreditoApiApplication.java
@@ -1,4 +1,4 @@
-package br.com.exemplo.credito;
+package br.com.exemplo.credito; 
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
